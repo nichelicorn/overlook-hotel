b802340a6adc3b383f5a8fc40663b9ad04b60d6a
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 17:31:34 -0600

    🧪 Add users to test data

---
+++ src/tests/test-data.js
@@ -0,0 +1,37 @@
+const testUsers = [
+  { id: 3,
+    name: "Wendy Torrance"
+  },
+  { id: 7,
+    name: "Delbert Grady"
+  },
+  { id: 13,
+    name: "Dick Hallorann"
+  }
+]
+
+const testRooms = [
+  { number: 237,
+    roomType: ,
+    bidet: ,
+    bedSize: "",
+    numBeds: ,
+    costPerNight: ,
+  },
+  { number: ,
+    roomType: ,
+    bidet: ,
+    bedSize: "",
+    numBeds: ,
+    costPerNight: ,
+  },
+  { number: ,
+    roomType: ,
+    bidet: ,
+    bedSize: "",
+    numBeds: ,
+    costPerNight: ,
+  }
+]
+
+export { testUsers , testRooms }
