8b569c10eca3250f7081d60b67efb3d3415aecd4
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:23:08 -0600

    🧪 Add user initialization to beforeEach

--- test/guest-test.js
+++ test/guest-test.js
@@ -1,15 +1,16 @@
 import chai from 'chai';
 const expect = chai.expect;
 import { testGuests , testRooms, testBookings} from '../test/test-data';
-// import Guest from '../src/Guest';
+import Guest from '../src/Guest';
 
 describe('The Guest', () => {
 
   let guest1, guest2, guest3, room1, room2, room3, booking1, booking2, booking3, booking4, booking5, booking6;
 
-  console.log('testGuests <>>>', testGuests[0]);
   beforeEach(() => {
-    // user1 = new Guest(testGuests[0]);
+    user1 = new Guest(testGuests[0]);
+    user2 = new Guest(testGuests[1]);
+    user3 = new Guest(testGuests[2]);
   })
 
 })
