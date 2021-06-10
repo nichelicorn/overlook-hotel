0e80d4074803ea8475366ac020d4b02c047104fa
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:47:57 -0600

    🧪 Add describe skeleton for Room test

--- test/room-test.js
+++ test/room-test.js
@@ -13,4 +13,69 @@ describe('The Room', () => {
     room3 = new Room(testRooms[2]);
   })
 
+  // Room.constructor
+  describe('Room properties', () => {
+
+    it.skip('should store a room number', () => {
+
+    })
+
+    it.skip('should store the room number as a number', () => {
+
+    })
+
+    it.skip('should store a room type', () => {
+
+    })
+
+    it.skip('should store the room type as a string', () => {
+
+    })
+
+    it.skip('should know if the room has a bidet', () => {
+
+    })
+
+    it.skip('should default to not having a bidet', () => {
+
+    })
+
+    it.skip('should store the size of the bed', () => {
+
+    })
+
+    it.skip('should store the bed size as a string', () => {
+
+    })
+
+    it.skip('should store the number of beds in the room', () => {
+
+    })
+
+    it.skip('store the number of beds as a number', () => {
+
+    })
+
+    it.skip('should store the cost per night', () => {
+
+    })
+
+    it.skip('should store the cost per night as a number', () => {
+
+    })
+
+    // it.skip('should store all dates the room is booked', () => { // this is not in the API data
+    //
+    // })
+
+    // it.skip('should store the booked dates in an array', () => {
+    //
+    // })
+
+    // it.skip('should store the dates as strings', () => {
+    //
+    // })
+
+  })
+
 })
