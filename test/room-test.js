4dd9686ab28186aedb813feade93432d35a78a84
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:41:48 -0600

    🧪 Add beforeEach to Room test

--- test/room-test.js
+++ test/room-test.js
@@ -1,9 +1,16 @@
 import chai from 'chai';
 const expect = chai.expect;
-import { testUsers , testRooms, testBookings} from '../test/test-data';
+import { testRooms } from '../test/test-data';
+import Room from '../src/Room';
 
 describe('The Room', () => {
 
+  let room1, room2, room3;
 
+  beforeEach(() => {
+    room1 = new Room(testRooms[0]);
+    room2 = new Room(testRooms[1]);
+    room3 = new Room(testRooms[2]);
+  })
 
 })
