b51ff683c6d21afe38a2b4669abe6cca9e7a379e
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:50:15 -0600

    🧪 Add beforeEach to Booking test

--- test/booking-test.js
+++ test/booking-test.js
@@ -1,9 +1,18 @@
 import chai from 'chai';
 const expect = chai.expect;
-import { testUsers , testRooms, testBookings} from '../test/test-data';
+import { testBookings } from '../test/test-data';
 
 describe('The Booking', () => {
 
+  let booking1, booking2, booking3, booking4, booking5, booking6;
 
+  beforeEach(() => {
+    booking1 = new Booking(testBookings[0]);
+    booking2 = new Booking(testBookings[1]);
+    booking3 = new Booking(testBookings[2]);
+    booking4 = new Booking(testBookings[3]);
+    booking5 = new Booking(testBookings[4]);
+    booking6 = new Booking(testBookings[5]);
+  })
 
 })
