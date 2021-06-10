2fd09ae13665c7868a18d1d3e47c9fd43ccce8fe
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:24:39 -0600

    🧪 Add rooms to Guest test

--- test/guest-test.js
+++ test/guest-test.js
@@ -7,10 +7,14 @@ describe('The Guest', () => {
 
   let guest1, guest2, guest3, room1, room2, room3, booking1, booking2, booking3, booking4, booking5, booking6;
 
+  console.log(testRooms[0])
   beforeEach(() => {
     user1 = new Guest(testGuests[0]);
     user2 = new Guest(testGuests[1]);
     user3 = new Guest(testGuests[2]);
+    room1 = testRooms[0];
+    room2 = testRooms[1];
+    room3 = testRooms[2];
   })
 
 })
