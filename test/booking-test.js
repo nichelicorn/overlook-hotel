6337de72babcc4014a30b65b93e11bab9cd0a679
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:57:59 -0600

    🧪 Add describe skeleton to Booking test

--- test/booking-test.js
+++ test/booking-test.js
@@ -15,4 +15,49 @@ describe('The Booking', () => {
     booking6 = new Booking(testBookings[5]);
   })
 
+  // Booking.constructor
+  describe('Booking properties', () => {
+
+    it.skip('should store an id', () => {
+
+    })
+
+    it.skip('should store the id as a string', () => {
+
+    })
+
+    it.skip('should store a user id', () => {
+
+    })
+
+    it.skip('should store the user id as a number', () => {
+
+    })
+
+    it.skip('should store a date', () => {
+
+    })
+
+    it.skip('should store the date as a string', () => {
+
+    })
+
+    it.skip('should store a room number', () => {
+
+    })
+
+    it.skip('should store the room number as a number', () => {
+
+    })
+
+    it.skip('should store room service charges', () => {
+
+    })
+
+    it.skip('should store room service charges in an array', () => {
+
+    })
+
+  })
+
 })
