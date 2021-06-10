05dade3619a82648eeaa202748b12ef4b7ccff82
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 19:10:53 -0600

    ♻️  Delete sample test

--- test/sample-test.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-const expect = chai.expect;
-import { testUsers , testRooms, testBookings} from '../test/test-data';
-
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
-});
