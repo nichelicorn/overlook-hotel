09f0a618fef3a9b8f866537e85b7180b2d8550f2
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-10 16:45:20 -0600

    ✨ Customer data logging to console

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,10 +1,17 @@
 const apiCalls = () => {
   const allGuestData = fetch('http://localhost:3001/api/v1/customers')
   .then(response => response.json())
+  .then(allGuestData => console.log(allGuestData))
   .then(allGuestData => { return allGuestData })
 
-  
 
+  // return Promise.all(allGuestData)
+  //   .then(data => {
+  //     const allData = {}
+  //     // allData.allGuestData = data
+  //     return allData
+  //   })
+    .catch(err => console.log('Error 🧟 ', err))
 }
 
 export { apiCalls }
--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,17 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import './api-calls.js'
+import { apiCalls } from './api-calls.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+window.addEventListener('load', onLoad);
+
+function onLoad() {
+  console.log('allGuestData 👇');
+  apiCalls()
+  // .then(allData => console.log(allData))
+}
