b266d6c216ea1fe7a65a5bcf8a31a4eff995425f
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-09 18:26:04 -0600

    🧪 Add bookings to beforeEach

--- test/guest-test.js
+++ test/guest-test.js
@@ -7,7 +7,6 @@ describe('The Guest', () => {
 
   let guest1, guest2, guest3, room1, room2, room3, booking1, booking2, booking3, booking4, booking5, booking6;
 
-  console.log(testRooms[0])
   beforeEach(() => {
     user1 = new Guest(testGuests[0]);
     user2 = new Guest(testGuests[1]);
@@ -15,6 +14,12 @@ describe('The Guest', () => {
     room1 = testRooms[0];
     room2 = testRooms[1];
     room3 = testRooms[2];
+    booking1 = testBookings[0];
+    booking2 = testBookings[1];
+    booking3 = testBookings[2];
+    booking4 = testBookings[3];
+    booking5 = testBookings[4];
+    booking6 = testBookings[5];
   })
 
 })
