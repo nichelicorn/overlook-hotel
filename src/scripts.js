82104614a4e37a5244d33caabca0aab999b192df
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-10 17:03:10 -0600

    ♻️  Comment out console.logs

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,7 @@ import { apiCalls } from './api-calls.js'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
 
 window.addEventListener('load', onLoad);
 
