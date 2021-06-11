b201b047eb7dd84dc7cd91ea158ca8039a7398e8
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-11 16:59:41 -0600

    ♻️  Refactor Room instantiation to expect single data input

--- test/room-test.js
+++ test/room-test.js
@@ -3,41 +3,53 @@ const expect = chai.expect;
 import { testRooms } from '../test/test-data';
 import Room from '../src/Room';
 
-describe('The Room', () => {
+describe.only('The Room', () => {
 
   let room1, room2, room3;
 
   beforeEach(() => {
-    room1 = new Room(testRooms[0]);
-    room2 = new Room(testRooms[1]);
-    room3 = new Room(testRooms[2]);
+    room1 = new Room(testRooms[0].id, testRooms[0].roomType, testRooms[0].bidet, testRooms[0].bedSize, testRooms[0].numBeds, testRooms[0].costPerNight);
+    room2 = new Room(testRooms[1].id, testRooms[1].roomType, testRooms[1].bidet, testRooms[1].bedSize, testRooms[1].numBeds, testRooms[1].costPerNight);
+    room3 = new Room(testRooms[2].id, testRooms[2].roomType, testRooms[2].bidet, testRooms[2].bedSize, testRooms[2].numBeds, testRooms[2].costPerNight);
   })
 
   // Room.constructor
   describe('Room properties', () => {
 
     it.skip('should store a room number', () => {
-
+      expect(room1.number).to.equal(42);
+      expect(room2.number).to.equal(217);
+      expect(room3.number).to.equal(237);
     })
 
     it.skip('should store the room number as a number', () => {
-
+      expect(room1.number).to.be.a('number');
+      expect(room2.number).to.be.a('number');
+      expect(room3.number).to.be.a('number');
     })
 
     it.skip('should store a room type', () => {
-
+      expect(room1.type).to.equal('single room');
+      expect(room2.type).to.equal('suite');
+      expect(room3.type).to.equal('residential suite');
     })
 
     it.skip('should store the room type as a string', () => {
-
+      expect(room1.type).to.be.a('string');
+      expect(room2.type).to.be.a('string');
+      expect(room3.type).to.be.a('string');
     })
 
     it.skip('should know if the room has a bidet', () => {
-
+      expect(room1.bidet).to.be(false);
+      expect(room2.bidet).to.be(true);
+      expect(room3.bidet).to.be(true);
     })
 
     it.skip('should default to not having a bidet', () => {
+      let room4 = new Room(13, 'suite', 'queen', 1, 777.77)
 
+      expect(room4.bidet).to.be(false);
     })
 
     it.skip('should store the size of the bed', () => {
