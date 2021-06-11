63663cb308e97967540d5d5cf754ba9f66d07b29
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-11 12:43:26 -0600

    ✨ Add block for apiCalls.getBookingData

--- src/api-calls.js
+++ src/api-calls.js
@@ -10,12 +10,17 @@ const apiCalls = {
 
   getRoomData() {
     return fetch('http://localhost:3001/api/v1/rooms')
-      .then(response => response.json())
-      .then(allRoomData => { return allRoomData })
-      .catch(err => console.log('Error 🧟 : ', err))
-  }
+    .then(response => response.json())
+    .then(allRoomData => { return allRoomData })
+    .catch(err => console.log('Error 🧟 : ', err))
+  },
 
-  
+  getBookingData() {
+    return fetch('http://localhost:3001/api/v1/bookings')
+    .then(response => response.json())
+    .then(allBookingData => { return allBookingData })
+    .catch(err => console.log('Error 🧟 : ', err))
+  }
 
   //
   // const getBookingData = fetch('http://localhost:3001/api/v1/bookings')
