015cfa86eb5a5e31530414b7dcf7c3b3e5b0492b
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-11 17:30:56 -0600

    ♻️  Refactor beforeEach to expect single data inputs in Booking instantiation

--- test/booking-test.js
+++ test/booking-test.js
@@ -7,12 +7,12 @@ describe('The Booking', () => {
   let booking1, booking2, booking3, booking4, booking5, booking6;
 
   beforeEach(() => {
-    booking1 = new Booking(testBookings[0]);
-    booking2 = new Booking(testBookings[1]);
-    booking3 = new Booking(testBookings[2]);
-    booking4 = new Booking(testBookings[3]);
-    booking5 = new Booking(testBookings[4]);
-    booking6 = new Booking(testBookings[5]);
+    booking1 = new Booking(testBookings[0].userID, testBookings[0].date, testBookings[0].roomNumber);
+    booking2 = new Booking(testBookings[1].userID, testBookings[1].date, testBookings[1].roomNumber);
+    booking3 = new Booking(testBookings[2].userID, testBookings[2].date, testBookings[2].roomNumber);
+    booking4 = new Booking(testBookings[3].userID, testBookings[3].date, testBookings[3].roomNumber);
+    booking5 = new Booking(testBookings[4].userID, testBookings[4].date, testBookings[4].roomNumber);
+    booking6 = new Booking(testBookings[5].userID, testBookings[5].date, testBookings[5].roomNumber);
   })
 
   // Booking.constructor
