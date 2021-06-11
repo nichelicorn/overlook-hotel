becabde28b7bfb49907b2606397e5fa311acd86f
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-11 12:35:36 -0600

    ♻️  Refactor api-calls - guest data is now logging in the console

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,29 +1,47 @@
-const apiCalls = () => {
-  const allGuestData = fetch('http://localhost:3001/api/v1/customers')
+// const apiCalls = () => {
+const apiCalls = {
+
+  getGuestData() {
+    return fetch('http://localhost:3001/api/v1/customers')
+    // const getGuestData = fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
-    // .then(allGuestData => console.log('Guests', allGuestData))
+    // .then(allGuestData => console.log('All Guests <>>>', allGuestData))
+    // logs an object `{customers : [50]}` ; comment out line 8
     .then(allGuestData => { return allGuestData })
+    // comment out console.logs and it works?!?!?! what the hell, fetch?!
+    // .then(allGuestData => console.log('2 <>>>', allGuestData)); // undefined ; line 8 commented out, now logs an object `{customers : [50]}`
+    .catch(err => console.log('Error 🧟 ', err))
+  },
 
-  const allRoomData = fetch('http://localhost:3001/api/v1/rooms')
-    .then(response => response.json())
-    // .then(allRoomData => console.log('Rooms', allRoomData))
-    .then(allRoomData => { return allRoomData })
+  // const getRoomData = fetch('http://localhost:3001/api/v1/rooms')
+  //   .then(response => response.json())
+  //   // .then(allRoomData => console.log('Rooms', allRoomData))
+  //   .then(allRoomData => { return allRoomData })
+  //
+  // const getBookingData = fetch('http://localhost:3001/api/v1/bookings')
+  //   .then(response => response.json())
+  //   // .then(allBookingData => console.log('Bookings', allBookingData))
+  //   .then(allBookingData => { return allBookingData })
 
-  const allBookingData = fetch('http://localhost:3001/api/v1/bookings')
-    .then(response => response.json())
-    // .then(allBookingData => console.log('Bookings', allBookingData))
+  // return Promise.all([allGuestData, allRoomData, allBookingData])
+  //   .then(data => {
+  //     // const allData = data
+  //     const allData = {}
+  //     allData.allGuestData = data[0]
+  //     allData.allRoomData = data[1]
+  //     allData.allBookingData = data[2]
+  //     return allData
+  //     // return data
+  //   })
 
+  getAllData() {
+    let promises = [ apiCalls.getGuestData() ];
+    return Promise.all(promises)
+    .then(allData => { return allData })
+    .catch(error => console.log(`Error : 🧟 : ${error}`))
+  },
 
-  return Promise.all([allGuestData, allRoomData, allBookingData])
-    .then(data => {
-      const allData = {}
-      allData.allGuestData = data[0]
-      allData.allRoomData = data[1]
-      allData.allBookingData = data[2]
-      return allData
-    })
-
-  .catch(err => console.log('Error 🧟 ', err))
+  // .catch(err => console.log('Error 🧟 ', err))
 }
 
-export { apiCalls }
+export default apiCalls;
--- src/scripts.js
+++ src/scripts.js
@@ -3,17 +3,25 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import { apiCalls } from './api-calls.js'
+import apiCalls from './api-calls.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 // console.log('This is the JavaScript entry file - your code begins here.');
 
-window.addEventListener('load', onLoad);
+window.addEventListener('load', loadAPIData);
+console.log('apiCalls 👇');
+console.log(apiCalls);
 
-function onLoad() {
-  // console.log('allGuestData 👇');
-  apiCalls()
-  .then(allData => console.log('in the scripts file!', allData))
+function loadAPIData() {
+  // console.log('allGuestData 👇')
+  apiCalls.getGuestData()
+    .then(allGuestData => {
+      console.log('hello all guests!')
+      console.log(allGuestData)
+      // still returns undefined ; with all console.logs in api-calls this works to log guest data in the console from the scripts file; fetch is weird
+    })
+  // console.log('allData 👇')
+  // apiCalls.getAllData()
 }
