896f823b15d6a5114e6cafe0a79d25761194c4e8
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-13 10:39:22 -0600

    ♻️ Refactor beforeEach in hotel-test

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -9,30 +9,44 @@ import { testGuests, testUsers , testRooms, testBookings } from '../test/test-da
 
 describe('The Hotel', () => {
 
-  // let guest1, guest2, guest3, guest4, guests, room1, room2, room3, rooms,  booking1, booking2, booking3, booking4, booking5, booking6, bookings, hotel;
+  let guest1, guest2, guest3, guest4, guests, room1, room2, room3, rooms,  booking1, booking2, booking3, booking4, booking5, booking6, bookings, hotel;
 
   beforeEach(() => {
+    guest1 = new Guest(testGuests[0]);
+    guest2 = new Guest(testGuests[1]);
+    guest3 = new Guest(testGuests[2]);
+    guest4 = new Guest(testGuests[3]);
+    guests = [ guest1, guest2, guest3, guest4 ];
+    room1 = new Room(testRooms[0]);
+    room2 = new Room(testRooms[1]);
+    room3 = new Room(testRooms[2]);
+    rooms = [ room1, room2, room3 ];
+    booking1 = new Booking(testBookings[0]);
+    booking2 = new Booking(testBookings[1]);
+    booking3 = new Booking(testBookings[2]);
+    booking4 = new Booking(testBookings[3]);
+    booking5 = new Booking(testBookings[4]);
+    booking6 = new Booking(testBookings[5]);
+    bookings = [ booking1, booking1, booking3, booking4, booking5, booking6 ];
+    hotel = new Hotel(guests, rooms, bookings);
+
     // guest1 = new Guest(testGuests[0].id, testGuests[0].name);
     // guest2 = new Guest(testGuests[1].id, testGuests[1].name);
     // guest3 = new Guest(testGuests[2].id, testGuests[2].name);
     // guest4 = new Guest(testGuests[3].id, testGuests[3].name);
-    // guests = [ guest1, guest2, guest3 ];
     // room1 = new Room(testRooms[0].number, testRooms[0].roomType, testRooms[0].bidet, testRooms[0].bedSize, testRooms[0].numBeds, testRooms[0].costPerNight);
     // room2 = new Room(testRooms[1].number, testRooms[1].roomType, testRooms[1].bidet, testRooms[1].bedSize, testRooms[1].numBeds, testRooms[1].costPerNight);
     // room3 = new Room(testRooms[2].number, testRooms[2].roomType, testRooms[2].bidet, testRooms[2].bedSize, testRooms[2].numBeds, testRooms[2].costPerNight);
-    // rooms = [room1, room2, room3];
     // booking1 = new Booking(testBookings[0].userID, testBookings[0].date, testBookings[0].roomNumber);
     // booking2 = new Booking(testBookings[1].userID, testBookings[1].date, testBookings[1].roomNumber);
     // booking3 = new Booking(testBookings[2].userID, testBookings[2].date, testBookings[2].roomNumber);
     // booking4 = new Booking(testBookings[3].userID, testBookings[3].date, testBookings[3].roomNumber);
     // booking5 = new Booking(testBookings[4].userID, testBookings[4].date, testBookings[4].roomNumber);
     // booking6 = new Booking(testBookings[5].userID, testBookings[5].date, testBookings[5].roomNumber);
-    // bookings = [booking1, booking1, booking3, booking4, booking5, booking6 ];
-    // hotel = new Hotel(guests, rooms, bookings);
   })
 
   describe('Hotel properties', () => {
-    it.skip('should store all Guest data', () => {
+    it('should store all Guest data', () => {
       console.log('hotel', hotel);
       expect(hotel.allGuests).to.deep.equal(guests);
     })
