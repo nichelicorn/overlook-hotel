21febf591204282b074322b16b50fca1f3bfaf8e
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-17 17:25:58 -0600

    ♻️  Update test expectation for Hotel.viewAvailRooms

--- test/mock-hotel-test.js
+++ test/mock-hotel-test.js
@@ -100,7 +100,10 @@ describe.only('The Hotel', () => {
       expect(hotel.viewAvailRooms('2021/12/31')).to.deep.equal([testRooms[0], testRooms[1], testRooms[2]]);
     })
 
-    it.skip('should return a message if no rooms are available on the selected date', () => {
+    it('should return a message if no rooms are available on the selected date', () => {
+      hotel.createObjects(bookings);
+      hotel.createObjects(rooms);
+
       expect(hotel.viewAvailRooms('2021/10/31')).to.equal('We regret to say, there are no rooms available that evening');
     })
 
