80e612ee82d7985e4ffcc474e8a57d3ebc27c08a
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-24 14:45:19 -0600

    ♻️  Fix lint errors

--- src/api-calls.js
+++ src/api-calls.js
@@ -3,28 +3,36 @@ const apiCalls = {
   getGuestData() {
     return fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
-    .then(allGuestData => { return allGuestData })
+    .then(allGuestData => {
+      return allGuestData
+    })
     .catch(err => console.log('Error 🧟 ', err))
   },
 
   getRoomData() {
     return fetch('http://localhost:3001/api/v1/rooms')
     .then(response => response.json())
-    .then(allRoomData => { return allRoomData })
+    .then(allRoomData => {
+      return allRoomData
+    })
     .catch(err => console.log('Error 🧟 : ', err))
   },
 
   getBookingData() {
     return fetch('http://localhost:3001/api/v1/bookings')
     .then(response => response.json())
-    .then(allBookingData => { return allBookingData })
+    .then(allBookingData => {
+      return allBookingData
+    })
     .catch(err => console.log('Error 🧟 : ', err))
   },
 
   getAllData() {
     let promises = [ apiCalls.getGuestData(), apiCalls.getRoomData(), apiCalls.getBookingData() ]
     return Promise.all(promises)
-    .then(allData => { return allData })
+    .then(allData => {
+      return allData 
+    })
     .catch(err => console.log('Error 🧟 : ', err))
   },
 }
