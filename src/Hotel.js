074e8143038736242a362d4dceb84714ddd9992c
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-24 12:28:26 -0600

    📝 Update pseudo

--- src/Hotel.js
+++ src/Hotel.js
@@ -32,32 +32,24 @@ class Hotel {
     return filteredRooms;
   }
 
-  viewAvailRooms(date, roomType) { // missing a ROOM for the booking 🤦‍♀️
+  viewAvailRooms(date, roomType) {
     let availRooms = this.filterRoomsByType(roomType);
-
     console.log('filtered rooms <>>>', availRooms);
-
     // let availRooms = [];
     let roomSearch = this.allBookings.filter(booking => {
       let openRoom = availRooms.forEach(room => {
       // let openRoom = this.allRooms.forEach(room => {
-      // let openRoom = this.allRooms.forEach(room => {
-        // if (date === booking.date || room.number === booking.roomNumber) { // if the date requested is already booked  OR the room number requested is already booked =>
-        if (date === booking.date && room.number === booking.roomNumber) { // if the date requested is already booked  AND the room number requested is already booked =>
+        if (date === booking.date && room.number === booking.roomNumber) {
           console.log(`Room no. ${room.number} is booked on the selected date`);
         };
-        if (!availRooms.includes(room) && !(date === booking.date && room.number === booking.roomNumber)) { // if availRooms does NOT include the room  AND ( the date does not match the booking date  AND  the room.number does NOT match a booking for that date)
+        if (!availRooms.includes(room) && !(date === booking.date && room.number === booking.roomNumber)) {
           console.log(`Room no. ${room.number} should be available`);
           // availRooms.push(room); // push the room into the array
         }
-        // if (!(date === booking.date && room.number === booking.roomNumber) && !availRooms.includes(room)) {
-        //   console.log(`Room no. ${room.number} should be available`);
-        //   availRooms.push(room);
-        // };
       })
       console.log('openRoom <>>>', openRoom);
     });
-
+    console.log('availRooms after the ifs <>>>', availRooms); // if the room is found to have a booking, remove it from the availRooms array
     return availRooms;
   }
 
