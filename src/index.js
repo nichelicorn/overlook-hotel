9c2f5ea0ee50463f16de8abddbf29a394009d424
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-28 12:47:53 -0600

    ♻️  Update imports to correct fetch error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import apiCalls from './api-calls.js'
-import scripts from './scripts.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './scripts.js'
+// import './api-calls.js'
