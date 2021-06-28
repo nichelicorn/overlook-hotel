25af0af591e3f2f0fc53fc58b2345fdec461ca77
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-28 12:41:26 -0600

    ✨ Add index file and update webpack config

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,12 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
-import apiCalls from './api-calls.js'
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// // This is the JavaScript entry file - your code begins here
+// // Do not delete or rename this file ********
+//
+// // An example of how you tell webpack to use a CSS (SCSS) file
+// import './css/base.scss';
+// import apiCalls from './api-calls.js'
+//
+// // An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// import './images/turing-logo.png'
 
 
 // 🌐 Global Variables
