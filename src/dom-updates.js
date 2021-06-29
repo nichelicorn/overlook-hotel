978c676cba408bacdc66d0d2d8e7e75348263e83
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-29 09:33:08 -0600

    ♻️  Work on DOM updates and add export to DOM updates file

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,1 +1,10 @@
 console.log('hello DOM udpates!')
+
+const domUpdates = {
+
+  guestAmtSpent() {
+    const guestSpending = document.getElementById('guestSpending');
+    // display hotel.activeUser.totalSpent
+  }
+
+}
