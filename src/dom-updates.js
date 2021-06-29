81bf877f689fc649547a7ac0a1c79462f7b9c8bc
Nichele Dunn <the7yearitch@hotmail.com>
2021-06-29 09:51:40 -0600

    ♻️  Delete console.logs

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -10,16 +10,12 @@ const welcomeMsg = document.getElementById('welcomeMsg');
 const domUpdates = {
 
   guestWelcomeMsg(name) {
-    let firstName = name.split(' ')[0];
-    // console.log(firstName);
+    const firstName = name.split(' ')[0];
     welcomeMsg.innerText = `Welcome to the Overlook, ${firstName}.`;
   },
 
   guestAmtSpent(spendings) {
-    // console.log('spendings DOM <>>>', spendings);
-    // guestSpending.innerText = `Hi!`;
     guestSpending.innerText = `You have spent $${spendings.toFixed(2)} since your first stay at the Overlook.`
-    // display hotel.activeUser.totalSpent
   },
 
 }
