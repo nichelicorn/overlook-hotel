2b2f8562f90a6cd310f9cfbf267544f645060f66
Nichele Dunn <the7yearitch@hotmail.com>
2021-07-01 15:03:16 -0600

    💄 Update booking card styles

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,12 +1,11 @@
-console.log('hello DOM udpates!');
-
-
+// console.log('hello DOM udpates!');
 
 // Query Selectors
 const allBookingsSec = document.getElementById('allBookingsSec');
 const guestSpending = document.getElementById('guestSpending');
 const welcomeMsg = document.getElementById('welcomeMsg');
 
+// DOM Update Functions
 const domUpdates = {
 
   guestWelcomeMsg(name) {
@@ -24,8 +23,8 @@ const domUpdates = {
     bookings.forEach(booking => {
       console.log('booking <>>>', booking);
       allBookingsSec.insertAdjacentHTML("beforeend",
-        `<article class="booking-card">
-          <p>Room no. ${booking.roomNumber}</p>
+        `<article class="booking-card" id="booking.id">
+          <p class="room-no">Room no. ${booking.roomNumber}</p>
           <img src="" alt="A posh hotel room">
           <p>Spent $${booking.nightlyRoomCharge} this visit</p>
         </article>`
